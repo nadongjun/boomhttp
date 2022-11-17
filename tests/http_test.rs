09bba0d938335db4cb5451d5f28bafc9f9c6143c
Dongjun Na <kmu5544616@gmail.com>
2022-11-17 16:57:30 +0900

    Added http-unit-test

--- tests/http_test.rs
+++ tests/http_test.rs
@@ -1,19 +1,20 @@
 use httpmock::prelude::*;
-use isahc::get;
+use isahc::{get, post, delete, put};
 
 #[test]
-fn simple_test() {
-    http::init_http_server();
-
-    let server = MockServer::connect("localhost:8080");
-
-    let hello_mock = server.mock(|when, then|{
-        when.path("configuration/config.json");
-        then.status(200);
+fn call_test() {
+    let server = MockServer::start();
+    let hello_mock = server.mock(|when, then| {
+        when.method(GET).path("/configuration/config.json");
+        then.status(200).header("content-type", "config/json").body("test");
     });
+    
 
-    let response = get(server.url("configuration/config.json")).unwrap();
+    let response_get = get(server.url("/configuration/config.json")).unwrap();
+    /*let response_post = post(server.url(url), "test").unwrap();
+    let response_delete = delete(server.url(url)).unwrap();
+    let response_put = put(server.url(url), "test").unwrap();*/
 
     hello_mock.assert();
-    assert_eq!(response.status(), 200);
+    assert_eq!(response_get.status(), 200);
 }
